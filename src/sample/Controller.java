59cef298a5d52feb2f80665bca1736109f8c7116
Luiz Roseiro <luuroseiro@gmail.com>
2017-03-02 19:58:44 -0300

    Leitura e extração de arquivo setup

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,12 @@
 package sample;
 
-import javafx.event.ActionEvent;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.MenuItem;
 import javafx.fxml.FXML;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.FileChooser;
 
 import java.io.File;
@@ -15,23 +19,31 @@ public class Controller {
     @FXML
     private MenuItem lineButton;
 
-    public void SetupButton(ActionEvent event) {
+    @FXML
+    public Canvas canvas;
+
+    public String[] SetupButton() {
         FileChooser fileChooserSetup = new FileChooser();
         fileChooserSetup.setInitialDirectory(new File("C:\\Users\\lf_ro\\Downloads"));
         fileChooserSetup.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Arquivo de texto", "*.txt"));
         File setupFile = fileChooserSetup.showOpenDialog(null);
 
         String pathSetup = setupFile.getAbsolutePath();
+        String[] arraySetup = null;
+        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
 
         try {
             ReadFile file = new ReadFile(pathSetup);
-            String[] arraySetup = file.OpenFile();
+            arraySetup = file.OpenFile();
+            drawGuiches(graphicsContext, modificaString(arraySetup[1]));
         } catch (IOException e) {
             System.out.println("Erro ao ler arquivo!");
         }
+
+        return arraySetup;
     }
 
-    public void LineButton(ActionEvent event) {
+    public void LineButton() {
         FileChooser fileChooserLine = new FileChooser();
         fileChooserLine.setInitialDirectory(new File("C:\\Users\\lf_ro\\Downloads"));
         fileChooserLine.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Arquivo de texto", "*.txt"));
@@ -42,11 +54,21 @@ public class Controller {
         try {
             ReadFile file = new ReadFile(pathLine);
             String[] arrayLine = file.OpenFile();
-
-            System.out.println(arrayLine[0]);
         } catch (IOException e) {
             System.out.println("Erro ao ler arquivo!");
         }
     }
 
+    public String[] modificaString(String oldString) {
+        String[] newString = oldString.split("");
+        return newString;
+    }
+
+    public void drawGuiches(GraphicsContext gc, String[] relacaoGuiches) {
+        gc.setFill(Color.RED);
+        gc.setStroke(Color.BLUE);
+        gc.setLineWidth(5);
+        gc.strokeLine(40,10,10,40);
+        gc.fillOval(10,60,30,30);
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,31 +2,31 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuItem;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import java.io.IOException;
 
-class User {
-    public int numberUser;
-    public int arriveUser;
-    public String guiches;  
-};
+import java.io.IOException;
 
 public class Main extends Application {
 
-    public static void main(String[] args) throws IOException {
-        launch(args);
-    }
-
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("sample.fxml"));
+        Controller controller = loader.getController();
+
+        StackPane root = loader.load();
         primaryStage.setTitle("Project San Francisco");
-        primaryStage.setScene(new Scene(root, 1100, 650));
+        primaryStage.setScene(new Scene(root, 1000, 650));
+        primaryStage.setResizable(false);
         primaryStage.show();
+    }
+
+    public static void main(String[] args) throws IOException {
+        launch(args);
     }
 }
